2889778d9b9c54f5d8c8deba133d94b6edb74149
Muhammad Rashid <m.rashid212823@gmail.com>
2025-03-10 05:42:58 +0500

    new ui

--- ui-url-shortner/src/components/input/input-comp.tsx
+++ ui-url-shortner/src/components/input/input-comp.tsx
@@ -32,23 +32,65 @@ export default function InputComp() {
 
   return (
     <div className={styles.main}>
-      <h1>URL Shortner</h1>
-      <label htmlFor="urlShortner">Enter URL:</label>
+      <h1 className={styles.heading}>URL Shortner</h1>
+      <label htmlFor="urlShortner" className={styles.inplabel}>
+        Enter URL
+      </label>
       <input
+        className={styles.input}
         type="text"
-        name=""
         id="urlShortner"
-        placeholder="https://www.youtube.com"
+        placeholder="https://www.example.com"
         value={originalUrl}
         onChange={(e) => setOriginalUrl(e.target.value)}
       />
-      <ButtonComp btnlabel={"Shorten"} btnhandler={() => shortenHandler()} />
+      <div className={styles.button}>
+        <svg
+          height="24"
+          width="24"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M0 0h24v24H0z" fill="none"></path>
+          <path
+            d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+            fill="currentColor"
+          ></path>
+        </svg>
+        <ButtonComp btnlabel={"Launch"} btnhandler={shortenHandler} />
+      </div>
 
-      {/* Show loading state while the URL is being shortened */}
-      {isLoading && <p>Loading...</p>}
+      {/* Loader display during URL shortening */}
+      {isLoading && (
+        <div className={styles.loader}>
+          <div className={styles["loading-text"]}>
+            Loading<span className={styles.dot}>.</span>
+            <span className={styles.dot}>.</span>
+            <span className={styles.dot}>.</span>
+          </div>
+          <div className={styles["loading-bar-background"]}>
+            <div className={styles["loading-bar"]}>
+              <div className={styles["white-bars-container"]}>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+                <div className={styles["white-bar"]}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
-      {/* Show shortened URL once it is available */}
-      {shortenedUrl && <ShortenURLdiv shortenedUrl={shortenedUrl} />}
+      {/* Show shortened URL once available */}
+      <div className={styles.urlCenter}>
+        {shortenedUrl && <ShortenURLdiv shortenedUrl={shortenedUrl} />}
+      </div>
     </div>
   );
 }
